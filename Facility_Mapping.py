0621ac63053661ef5c84db2771af46d58386daf5
Alvin-igobwa <53784550+Alvin-igobwa@users.noreply.github.com>
2024-05-08 12:46:56 +0300

    adding all new changes plus optimized code

--- Facility_Mapping.py
+++ Facility_Mapping.py
@@ -9,8 +9,6 @@ import math
 APP_TITLE = "Kenya health Facilities Mapping"
 APP_SUBTITLE = "The map shows all of Kenya's health facilities with their  corresponding Levels and highlights Level 4"
 
-
-@st.cache_data
 def load_data(string:str):
     """
     Load the data from the csv file
@@ -23,35 +21,7 @@ def load_data(string:str):
     return hospitals_gvt
 
 
-def haversine_distance(coord1, coord2):
-    """
-    Calculate the Haversine distance between two sets of (lat, lon) coordinates in kilometers.
-    """
-    R = 6371.0  # Earth radius in kilometers
-
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
-
-    dlat = math.radians(lat2 - lat1)
-    dlon = math.radians(lon2 - lon1)
-
-    a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    return R * c
-
-def hospitals_within_radius(hospital, other_hospitals, radius_km):
-    """
-    Find how many other hospitals are within a given radius of the hospital.
-    """
-    count = 0
-    for other_hospital in other_hospitals:
-        if haversine_distance((hospital['latitude'], hospital['longitude']), (other_hospital['latitude'], other_hospital['longitude'])) <= radius_km:
-            count += 1
-    return count
-
-
-def display_map(hospy:pd.DataFrame, hospitals_gvt:pd.DataFrame,kms_chooser:int):
+def display_map():
     
     attr = (
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
@@ -60,50 +30,32 @@ def display_map(hospy:pd.DataFrame, hospitals_gvt:pd.DataFrame,kms_chooser:int):
     tiles = "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
     center = [-0.023559, 37.9061928]
     map_kenya = folium.Map(location=center,tiles=tiles,attr=attr, zoom_start=6 )
+
+    return map_kenya
+
+mapper = display_map()
+
+def plot_points(point):
+    folium.Circle(location=[point.latitude, point.longitude],
+                        radius=5).add_to(mapper)
     
-    for index, hospitals_gvt in hospitals_gvt.iterrows():
-        location = [hospitals_gvt['latitude'], hospitals_gvt['longitude']]
-        folium.Circle(location, 
-                            radius=5,
-                            popup = f"Name: {hospitals_gvt.facility} Keph Level: {hospitals_gvt.keph_level}").add_to(map_kenya)
-        
+def all_points(hospy,km_chooser):
+    
+    
     for index, hospy in hospy.iterrows():
         location = [hospy['latitude'], hospy['longitude']]
         icon=folium.Icon(color='green')
         folium.Marker(location, 
                       icon = icon,
-                      popup = f"Name: {hospy.facility} Keph Level: {hospy.keph_level}").add_to(map_kenya)
+                      popup = f"Name: {hospy.facility} Keph Level: {hospy.keph_level}").add_to(mapper)
         
         folium.Circle(location,
-                    radius=kms_chooser*1000,
+                    radius=km_chooser*1000,
                     color = "green",
-                    popup = f"{kms_chooser} Radius, Name: {hospy.facility}, Keph Level: {hospy.keph_level}").add_to(map_kenya)
+                    popup = f"{km_chooser}Radius, Name: {hospy.facility}, Keph Level: {hospy.keph_level}").add_to(mapper)
     
-    st_map = st_folium(map_kenya)
-    return st_map
+ 
 
-
-@st.cache_data
-def distances(hospitals_gvt:pd.DataFrame, facility_level, kms_chooser):
-    level_4_hospitals = hospitals_gvt[hospitals_gvt['keph_level'] == facility_level]
-    # Filter out Level 2 and Level 3 hospitals
-    level_2_3_hospitals = hospitals_gvt[(hospitals_gvt['keph_level'] == "Level 2") | (hospitals_gvt['keph_level'] == "Level 3")]
-
-    # Find the Level 4 hospitals that do not have any Level 2 or Level 3 hospitals within 10 km
-    hospitals_no_level_2_3_nearby = []
-
-    for _, level_4_hospital in level_4_hospitals.iterrows():
-        if hospitals_within_radius(level_4_hospital, level_2_3_hospitals.to_dict('records'),kms_chooser) == 0:
-            hospitals_no_level_2_3_nearby.append(level_4_hospital['facility'])
-
-    # Print out the results
-    print("Level 4 hospitals without Level 2 or Level 3 hospitals within 10 km:")
-    hosis = []
-    for hospital in hospitals_no_level_2_3_nearby:
-        hosis.append(hospital)
-    
-    return hosis,hospitals_no_level_2_3_nearby
-    
 
 
 def main():
@@ -112,44 +64,72 @@ def main():
     st.subheader(APP_SUBTITLE)
 
     # LOAD DATA
-    hospitals_gvt = load_data("hospital geo_codes_clean.csv")
+    hospitals_gvt = load_data("hospital geo_codes_cleanest.csv")
 
     # st.selectbox("Select the Level interested in seeing", hospitals_gvt['keph_level'].unique())
 
     kms = [10,15,20]
-    kms_chooser = st.selectbox('Select the number of Kilometres for the radius', kms)
-
-
     facility_level = 'Level 4'
-    # county = 'Narok'                                                  to be added if need be
 
+    kms_chooser = st.sidebar.selectbox('Select the number of Kilometres for the radius', kms)
+
+    select_all = st.sidebar.checkbox('Select All Counties')
+    if select_all:
+        county = hospitals_gvt['county'].unique()
+        hospy = hospitals_gvt[(hospitals_gvt['keph_level']==facility_level)& (hospitals_gvt['county'].isin(county))]
+    else:
+        county = st.sidebar.selectbox('Choose a County', hospitals_gvt['county'].unique())
+        hospy = hospitals_gvt[(hospitals_gvt['keph_level']==facility_level)& (hospitals_gvt['county']==county)]
+    # county = st.sidebar.selectbox('Choose a County', hospitals_gvt['county'].unique())
+    # subcounty = st.sidebar.multiselect('Choose a Sub County', hospitals_gvt['sub_county'].unique())
     
-
-    hospy = hospitals_gvt[hospitals_gvt['keph_level']==facility_level]
-    metric_title = f"Number of Health Facilities of {facility_level} is:"
+    # county = 'Narok'                                                  to be added if need be
+    metric_title = f"{facility_level} Facilities:"
     metric_title2 = f"Total Number of Health Facilities is:"
+
+    if kms_chooser == 10:
+        dd = hospy['10km_to_level4'].sum()
+        hh = hospy[hospy['10km_to_level4'] != 0]['facility']
+        hh.index = range(1, hh.shape[0]+1)
+
+    elif kms_chooser == 15:
+        dd = hospy['15km_to_level4'].sum()
+        hh = hospy[hospitals_gvt['15km_to_level4'] != 0]['facility']
+        hh.index = range(1, hh.shape[0]+1)
+    elif kms_chooser == 20:
+        dd = hospy['20km_to_level4'].sum()
+        hh = hospy[hospy['20km_to_level4'] != 0]['facility'].reset_index()
+        hh.index = range(1, hh.shape[0]+1)
+
+
+    metric_title3= f"""No of Level 4 Hospitals without Level 2 or 3 within {kms_chooser} radius are:"""
+    
+    st.subheader(f"The Level 4 Health Facilities that do not have a level 2 or 3 health facility within the {kms_chooser}kms : are")
+    st.write(hh)
+    
     
     st.subheader(f"Facts")
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
     with col1:
         st.metric(metric_title2, "{:,}".format(hospitals_gvt.shape[0]))
     with col2:
         st.metric(metric_title, "{:,}".format(hospy.shape[0]))
+    with col3:
+        st.metric(metric_title3, dd)
 
 
     # DISPLAY FILTERS AND MAP
     
     st.subheader(f"Map of {facility_level} Health Facilities")
-    display_map(hospy,hospitals_gvt, kms_chooser)
+    hospitals_gvt.apply(plot_points, axis =1)
+    all_points(hospy,kms_chooser)
+    st_folium(mapper)
+    
+
+
 
     
-    # DISPLAY METRICS
-
-
-    dd = distances(hospitals_gvt, facility_level, kms_chooser)
-    st.write(dd[0])
-    metric_title3= f"The number of Level 4 Hospitals that have no Level 2 or 3 within a {kms_chooser} radius are:"
-    st.metric(metric_title3, len(dd[1]))
+    #
 
 if __name__ == "__main__":
     main()
